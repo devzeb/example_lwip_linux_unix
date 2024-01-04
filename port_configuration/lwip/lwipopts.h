43494861acf7d622c251fe093a4d558e6af0a5d8
Sebastian Michel <sebastian.michel96@gmail.com>
2024-01-04 19:46:12 +0100

    added additional lwip options (errno, debugging)

--- port_configuration/lwip/lwipopts.h
+++ port_configuration/lwip/lwipopts.h
@@ -1,6 +1,62 @@
 #ifndef LWIP_LWIPOPTS_H
 #define LWIP_LWIPOPTS_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// use the errno.h from the standard library.
+// this is mandatory for RTOS based systems, because only then it is
+#define LWIP_ERRNO_STDINCLUDE 1
+
+// debugging
+#define LWIP_DEBUG 1
+
+#define LWIP_DBG_MIN_LEVEL LWIP_DBG_LEVEL_ALL
+
+// debug options from lwip/opts.h
+#define ETHARP_DEBUG LWIP_DBG_ON
+#define NETIF_DEBUG LWIP_DBG_ON
+#define PBUF_DEBUG LWIP_DBG_ON
+#define API_LIB_DEBUG LWIP_DBG_ON
+#define API_MSG_DEBUG LWIP_DBG_ON
+#define SOCKETS_DEBUG LWIP_DBG_ON
+#define ICMP_DEBUG LWIP_DBG_ON
+#define IGMP_DEBUG LWIP_DBG_ON
+#define INET_DEBUG LWIP_DBG_ON
+#define IP_DEBUG LWIP_DBG_ON
+#define IP_REASS_DEBUG LWIP_DBG_ON
+#define RAW_DEBUG LWIP_DBG_ON
+#define MEM_DEBUG LWIP_DBG_ON
+#define MEMP_DEBUG LWIP_DBG_ON
+#define SYS_DEBUG LWIP_DBG_ON
+#define TIMERS_DEBUG LWIP_DBG_ON
+#define TCP_DEBUG LWIP_DBG_ON
+#define TCP_INPUT_DEBUG LWIP_DBG_ON
+#define TCP_FR_DEBUG LWIP_DBG_ON
+#define TCP_RTO_DEBUG LWIP_DBG_ON
+#define TCP_CWND_DEBUG LWIP_DBG_ON
+#define TCP_WND_DEBUG LWIP_DBG_ON
+#define TCP_OUTPUT_DEBUG LWIP_DBG_ON
+#define TCP_RST_DEBUG LWIP_DBG_ON
+#define TCP_QLEN_DEBUG LWIP_DBG_ON
+#define UDP_DEBUG LWIP_DBG_ON
+#define TCPIP_DEBUG LWIP_DBG_ON
+#define SLIP_DEBUG LWIP_DBG_ON
+#define DHCP_DEBUG LWIP_DBG_ON
+#define AUTOIP_DEBUG LWIP_DBG_ON
+// #define DHCP_DEBUG LWIP_DBG_ON //  WARNING ! do not enable DHCP_DEBUG, this makes the arp announcement fail
+#define IP6_DEBUG LWIP_DBG_ON
+#define DHCP6_DEBUG LWIP_DBG_ON
+
+// debug options from other lwip header files
+#define PPP_DEBUG LWIP_DBG_ON
+#define DNS_DEBUG LWIP_DBG_ON
+
+#define LWIP_DBG_TYPES_ON ( LWIP_DBG_ON | LWIP_DBG_TRACE | LWIP_DBG_STATE | LWIP_DBG_FRESH | LWIP_DBG_HALT )
+
+
 /*
    -----------------------------------------------
    ---------- Platform specific locking ----------
@@ -386,6 +442,10 @@
 #if !NO_SYS
 void sys_check_core_locking(void);
 #define LWIP_ASSERT_CORE_LOCKED()  sys_check_core_locking()
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif /* LWIP_LWIPOPTS_H */
