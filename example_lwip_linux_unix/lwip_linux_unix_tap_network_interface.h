bfe7215e39e64e83f7cd7630509fb7cbe23b114a
Sebastian Michel <sebastian.michel96@gmail.com>
2024-01-05 09:42:31 +0100

    fixed include header

--- example_lwip_linux_unix/lwip_linux_unix_tap_network_interface.h
+++ example_lwip_linux_unix/lwip_linux_unix_tap_network_interface.h
@@ -1,7 +1,7 @@
 #ifndef LWIP_LINUX_UNIX_TAP_NETWORK_INTERFACE_H
 #define LWIP_LINUX_UNIX_TAP_NETWORK_INTERFACE_H
 
-#include "netif/tapif.h"
+#include "lwip/netif.h"
 
 void init_default_netif(const ip4_addr_t* ipaddr, const ip4_addr_t* netmask, const ip4_addr_t* gw);
 
